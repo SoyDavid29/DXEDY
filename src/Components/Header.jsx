d980b9eaf21f425ea421caf281cee2593464976e
DavidRodriguez <studiandoprogramacion@gmail.com>
2025-03-01 17:54:30 -0600

    starting to work on sideBar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,17 +1,16 @@
-export default function Header(){
-    return(
-      <header className="sideBar">
-        <div className="bar">
-            <h1 className="logo">DxEdyShoes</h1>
-            <li className="menu-links">
-                <ul>Social Media</ul>
-                <ul>About Us</ul>
-                <ul>Brands</ul>
-                <img className="car-logo" src="/img/car.png" alt="Carrito" />
-                
-            </li>
+export default function Header() {
+    return (
+        <header className="sidebar">
+            <div className="bar">
+                <h1 className="logo">DxEdyShoes</h1>
+                <li className="menu-links">
+                    <ul className="main-link" >Social Media</ul>
+                    <ul className="main-link" >About Us</ul>
+                    <ul className="main-link" >Brands</ul>
+                    <img className="car-logo" src="/img/car.png" alt="Carrito" />
+                </li>
 
-        </div>
-      </header>
+            </div>
+        </header>
     )
 }
