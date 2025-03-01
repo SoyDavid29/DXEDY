5fd932f6e157cf6443424c62121a045a5de5d3fc
DavidRodriguez <studiandoprogramacion@gmail.com>
2025-03-01 15:32:14 -0600

    archivos cargados

---
+++ src/Components/Header.jsx
@@ -0,0 +1,17 @@
+export default function Header(){
+    return(
+      <header className="sideBar">
+        <div className="bar">
+            <h1 className="logo">DxEdyShoes</h1>
+            <li className="menu-links">
+                <ul>Social Media</ul>
+                <ul>About Us</ul>
+                <ul>Brands</ul>
+                <img className="car-logo" src="/img/car.png" alt="Carrito" />
+                
+            </li>
+
+        </div>
+      </header>
+    )
+}
