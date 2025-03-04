531327b4bf012299819ae89ca2fdaac393dc0ff1
DavidRodriguez <studiandoprogramacion@gmail.com>
2025-03-03 20:21:57 -0600

    we start to work on target

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
 import Header from './Components/Header'
+import Target from './Components/Target'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
        <Header/>
-  
 
+       <Target/>
+       
     </>
   )
 }
---
+++ src/Components/Target.jsx
@@ -0,0 +1,8 @@
+export default function Target(){
+
+    return(
+     <div className="target">
+      <img className="img-shoes" src="/img/IN-DZ5485-105-1.jpg" alt="" />
+     </div>
+    )
+}
