030c28be0e5890719b402f51c0e91e9653127a65
DavidRodriguez <studiandoprogramacion@gmail.com>
2025-03-03 18:47:53 -0600

    data base was build

---
+++ src/data/db.js
@@ -0,0 +1,34 @@
+export const db = [
+    {
+        id: 1,
+        name: 'Nike-pink',
+        image: 'IN-DZ5485-105-1',
+        description: 'Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.',
+        price: '$1,899.00 MXN'
+    },
+
+    {
+        id: 2,
+        name: 'Nike-Retro',
+        image: 'tenis-nike-dunk-retro-laser-blue-in-DD1399-400-1',
+        description: 'Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.',
+        price: '$1,399.00 MXN'
+    },
+
+    {
+        id: 3,
+        name: 'Nike-Nuaxis',
+        image: 'W+NIKE+AIR+MAX+NUAXIS+SE',
+        description: 'Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.',
+        price: '$1,699.00 MXN'
+    },
+
+    {
+        id: 4,
+        name: 'Nike-Dunk',
+        image: 'W+NIKE+DUNK+LOW',
+        description: 'Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.',
+        price: '$1,999.00 MXN'
+    },
+
+]
