34f3617ad74dfb5618aa9f0ac0a94bf81522056b
DavidRodriguez <studiandoprogramacion@gmail.com>
2025-03-05 14:17:50 -0600

    make component target complete

--- src/Components/Target.jsx
+++ src/Components/Target.jsx
@@ -2,7 +2,13 @@ export default function Target(){
 
     return(
      <div className="target">
-      <img className="img-shoes" src="/img/IN-DZ5485-105-1.jpg" alt="" />
+      <img className="img-shoes" src="/img/IN-DZ5485-105-1.jpg" alt="nike photo" />
+      <h1>Nike-Chunk</h1>
+      <p>Details</p>
+      <div className="price">
+        <p>$1,850.00 mxn</p>
+        <img className="car-logo" src="/img/car.png" alt="" />
+      </div>
      </div>
     )
 }
