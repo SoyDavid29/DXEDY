51704f494005ec83134b74f52371c1eba5987931
DavidRodriguez <studiandoprogramacion@gmail.com>
2025-05-14 16:21:05 -0600

    slights changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,42 +7,40 @@ import { db } from './data/db' /* importancion de db en forma de objeto{} */
 
 function App() {
   const [data, setData] = useState(db) /* useState para actualizacion de base de datos */
-  
+
   const [cart, setCart] = useState([]) /* UseState para actualizar un array vacio "cart" */
 
-  const [open, setOpen ] = useState(false) /* useState globlal para mis componentes */
+  const [open, setOpen] = useState(false) /* useState globlal para mis componentes */
   const toggleCart = () => setOpen(prev => !prev); /* toggle function actualiza el estado */
 
+  function addToCart(item) { /* creamos una funcion intermedia para escribir elementos a nuestro carrito de compras */
+    const itemExist = cart.findIndex(shoes => shoes.id === item.id) /* verificamos si existe un elemento con finIndex (no mutable) crea un objeto temporal (puede llamarse como sea, en este caso "shoes"*/
+    /* si no existe en nuestro state retorna -1 con el metodo findIndex */
+    if (itemExist >= 0) {  /* condicion para si el elemento ya existe  */
+      const updateCart = [...cart] /* creamos una variable que es una copia del nuestro state cart" usando spread...operator */
+      updateCart[itemExist].quantity++ /* modificamos la copia del state  */
+      setCart(updateCart) /* actualizamos el state de la copia */
 
- 
-  function addToCart(item){ /* creamos una funcion intermedia para escribir elementos a nuestro carrito de compras */
-  const itemExist = cart.findIndex(shoes => shoes.id === item.id ) /* verificamos si existe un elemento con finIndex (no mutable) crea un objeto temporal (puede llamarse como sea, en este caso "shoes"*/
-                                                                  /* si no existe en nuestro state retorna -1 con el metodo findIndex */
-  if(itemExist >= 0){  /* condicion para si el elemento ya existe  */
-    const updateCart = [...cart] /* creamos una variable que es una copia del nuestro state cart" usando spread...operator */
-    updateCart[itemExist].quantity++ /* modificamos la copia del state  */
-    setCart(updateCart) /* actualizamos el state de la copia */
-
-   }
-   else{
-    item.quantity = 1 /* agregamos propiedad nueva al objeto *item* */
-    setCart([...cart, item]) /* crea un nuevo array en el viejo cart mas el nuevo item */
-   }
+    }
+    else {
+      item.quantity = 1 /* agregamos propiedad nueva al objeto *item* */
+      setCart([...cart, item]) /* crea un nuevo array en el viejo cart mas el nuevo item */
+    }
   }
 
   return (
     <>
-      
+
       <div className='header'>
-      <Header toggleCart={toggleCart} /> {/* pasamos como prop la variable toggle */}
-      <Cart 
-       open={open} /* pasamos como prop la variable de estado */
-       cart={cart}/> {/* vamos a sincronizar la tabla con nuestro state *cart* */}
+        <Header toggleCart={toggleCart} /> {/* pasamos como prop la variable toggle */}
+        <Cart
+          open={open} /* pasamos como prop la variable de estado */
+          cart={cart} /> {/* vamos a sincronizar nuestos arrays a la tabla con nuestro state osea nuestro STATE *cart* */}
       </div>
 
 
       <div className='target-container'>
-        {data.map((shoes) => ( /* se recorre un array y a cada objeto se le asigna el nombre */
+        {data.map((shoes) => ( /* se recorre un array y a cada objeto se le asigna el nombre de shoes */
           <Target
             key={shoes.id}
             shoes={shoes} /* nombreDelProp={parametros del objeto} */
--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,8 +1,8 @@
-export default function Cart({open, cart}) {
+export default function Cart({ open, cart }) { /* CONECTAMOS nuestro state cart nuestro carrito de compras */
 
-   // STATE DERIVADO
-   const isEmpty = () => cart.length === 0
-   
+  // STATE DERIVADO
+  const isEmpty = () => cart.length === 0;
+
   return (
 
     <nav className={`cart ${open ? "show" : ""}`}>
@@ -11,65 +11,67 @@ export default function Cart({open, cart}) {
       >
 
         <div id="carrito" className="table">
-          {isEmpty() ?(
-          <p className="text-cart">El carrito esta vacio</p>
+          {isEmpty() ? (
+            <p className="text-cart">El carrito esta vacio</p>
           ) : (
-          <table className="table-content">
-            <thead> {/* encabezado  */}
-              <tr> {/* fila del encabezado */}
-                <th>Producto</th> {/* celda */}
-                <th>Nombre</th>
-                <th>Precio</th>
-                <th>Cantidad</th>
-                <th></th>
-              </tr>
-            </thead>
+            <table className="table-content"> {/* aqui inicia nuestra tabla */}
+              <thead> {/* encabezado  */}
+                <tr> {/* fila del encabezado */}
+                  <th>Producto</th> {/* celda */}
+                  <th>Nombre</th>
+                  <th>Precio</th>
+                  <th>Cantidad</th>
+                  <th></th>
+                </tr>
+              </thead>
 
-            <tbody> {/* cuerpo de la tabla */}
-              {cart.map(shoes => ( /* el objeto shoes existe e itera sobre nuestra tabla mostrando sus caracteristicas como img, price, etc. */
+              <tbody> {/* cuerpo de la tabla */}
+                {cart.map(shoes => ( /* el objeto shoes existe e itera sobre nuestra tabla mostrando sus caracteristicas como img, price, etc. */
 
-              <tr key={shoes.id}> {/* fila 1  */}
-                <td> {/* celda */}
-                  <img className="img-item" 
-                  src={`/img/${shoes.image}.jpg`}
-                  alt="imagen guitarra"
-                  />
+                  <tr key={shoes.id}> {/* fila 1  */}
+                    <td> {/* celda */}
+                      <img className="img-item"
+                        src={`/img/${shoes.image}.jpg`}
+                        alt="imagen guitarra"
+                      />
 
-                </td>
-                <td>{shoes.name}</td> {/* para hecer dinamico agregamos caracteristicas de nuestro objeto {shoes} */}
-                <td className="price">
-                 {shoes.price}
-                </td>
-                <td className="buttons">
-                  <button
-                    type="button"
-                    className="btn-dark"
-                  >
-                    -
-                  </button>
-                  {shoes.quantity}
-                  <button
-                    type="button"
-                    className="btn-dark"
-                  >
-                    +
-                  </button>
-                </td>
-                <td>
-                  <button
-                    className="btn-danger"
-                    type="button"
-                  >
-                    X
-                  </button>
-                </td>
-              </tr>
+                    </td>
+                    <td>{shoes.name}</td> {/* para hecer dinamico agregamos caracteristicas de nuestro objeto {shoes} */}
+                    <td className="price">
+                      {shoes.price}
+                    </td>
+                    <td className="buttons">
+                      <button
+                        type="button"
+                        className="btn-dark"
+                      >
+                        -
+                      </button>
+                      {shoes.quantity}
+                      <button
+                        type="button"
+                        className="btn-dark"
+                      >
+                        +
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        className="btn-danger"
+                        type="button"
+                      >
+                        X
+                      </button>
+                    </td>
+                  </tr>
 
-               ))}
-               
-            </tbody>
-          </table>
+                ))}
+
+              </tbody>
+            </table>
           )}
+
+
           <p className="text-end">Total pagar: <span className="total-price">$899</span></p>
           <button className="btn-vaciar">Vaciar Carrito</button>
         </div>
